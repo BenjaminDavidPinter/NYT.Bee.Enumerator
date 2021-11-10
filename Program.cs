6f98a8d134741cb03068a6e3ed87b14bb0600018
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2021-11-10 13:24:24 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WindowsInput;
+
+namespace NYT.Bee.Enumerator
+{
+    class Program
+    {
+        public static int SystemWideDelay = 100;
+        public static int EnterKeyWait = 100;
+        public static List<Tuple<WindowsInput.Native.VirtualKeyCode, char>> todaysLetters = new List<Tuple<WindowsInput.Native.VirtualKeyCode, char>>()
+        {
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_A, 'a'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_G, 'g'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_H, 'h'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_I, 'i'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_K, 'k'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_W, 'w'),
+            Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_S, 's'),
+        };
+
+        public static Tuple<WindowsInput.Native.VirtualKeyCode, char> MustHaveLetter = Tuple.Create(WindowsInput.Native.VirtualKeyCode.VK_I, 'i');
+
+        public static List<string> dictionary = File.ReadAllLines("Dictionary.txt").ToList();
+
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(5000);
+            dictionary = dictionary.Distinct().ToList();
+            var thisDict = new List<string>(dictionary);
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                dictionary[i] = dictionary[i].ToLower();
+                if(!dictionary[i].All(x => todaysLetters.Select(y => y.Item2).Contains(x))
+                    || !dictionary[i].Contains(MustHaveLetter.Item2)
+                    || dictionary[i].Length < 4)
+                {
+                    thisDict.Remove(dictionary[i]);
+                }
+            }
+            thisDict.OrderBy(x => x.Length);
+           
+            InputSimulator sim = new InputSimulator();
+            foreach (var word in thisDict)
+            {
+                foreach (var charac in word)
+                {
+                    sim.Keyboard.KeyPress(todaysLetters.First(x => x.Item2 == charac).Item1);
+                    System.Threading.Thread.Sleep(SystemWideDelay);
+                }
+                sim.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
+                System.Threading.Thread.Sleep(EnterKeyWait);
+            }
+        }
+
+        public static Collection<String> GetWordsOfLength(int length)
+        {
+            Collection<String> eligibleWords = new Collection<string>();
+            foreach (var permus in ShuffleListExtension.CombinationsWithRepetition(todaysLetters
+                .Select(x => x.Item2.ToString()), length)
+                .Where(x => x.Length == length)
+                .Where(x => x.Contains(MustHaveLetter.Item2) && dictionary.Contains(x)))
+            {
+                eligibleWords.Add(permus);
+            }
+            return eligibleWords;
+        }
+    }
+}
---
+++ ShuffleListExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYT.Bee.Enumerator
+{
+    public static class ShuffleListExtension
+    {
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<String> CombinationsWithRepetition(IEnumerable<string> input, int length)
+        {
+            if (length <= 0)
+                yield return "";
+            else
+            {
+                foreach (var i in input)
+                    foreach (var c in CombinationsWithRepetition(input, length - 1))
+                        yield return i + c;
+            }
+        }
+    }
+}
